de141d0c92e7bc18a7da0eb12b9ad466967b981b
pengfeng015 <pf0715479@163.com>
2017-01-26 17:45:49 +0800

    Update views.py

--- testapp/views.py
+++ testapp/views.py
@@ -1,22 +1,32 @@
-#coding=utf-8
 
-import hashlib
+# -*- coding: utf-8 -*-
 
-import json
-
-from lxml import etree
-
-from django.utils.encoding import smart_str
-
+from __future__ import unicode_literals
+from django.http.response import HttpResponse, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
+from wechat_sdk import WechatBasic
+from wechat_sdk.exceptions import ParseError
+from wechat_sdk.messages import TextMessage
 
-from django.http import HttpResponse
+WECHAT_TOKEN = 'yuxuanyixiao'
 
-from auto_reply.views import auto_reply_main # 修改这里
+AppID = 'wx6c1c0226980f62e2'
+
+AppSecret = '5b29cdb78ab3cdd95972d7a9bee4cd24'
 
  
 
-WEIXIN_TOKEN = 'xuanxuanyixiao'
+# 实例化 WechatBasic
+
+wechat_instance = WechatBasic(
+
+    token=WECHAT_TOKEN,
+
+    appid=AppID,
+
+    appsecret=AppSecret
+
+)
 
  
 
@@ -24,48 +34,96 @@ WEIXIN_TOKEN = 'xuanxuanyixiao'
 
 def weixin_main(request):
 
-    """
-    所有的消息都会先进入这个函数进行处理，函数包含两个功能，
-    微信接入验证是GET方法，
-    微信正常的收发消息是用POST方法。
-    """
+    if request.method == 'GET':
 
-    if request.method == "GET":
+        # 检验合法性
 
-        signature = request.GET.get("signature", None)
+        # 从 request 中提取基本信息 (signature, timestamp, nonce, xml)
 
-        timestamp = request.GET.get("timestamp", None)
+        signature = request.GET.get('signature')
 
-        nonce = request.GET.get("nonce", None)
+        timestamp = request.GET.get('timestamp')
 
-        echostr = request.GET.get("echostr", None)
+        nonce = request.GET.get('nonce')
 
-        token = WEIXIN_TOKEN
+ 
 
-        tmp_list = [token, timestamp, nonce]
+        if not wechat_instance.check_signature(
 
-        tmp_list.sort()
+                signature=signature, timestamp=timestamp, nonce=nonce):
 
-        tmp_str = "%s%s%s" % tuple(tmp_list)
+            return HttpResponseBadRequest('Verify Failed')
 
-        tmp_str = hashlib.sha1(tmp_str).hexdigest()
+ 
 
-        if tmp_str == signature:
+        return HttpResponse(
 
-            return HttpResponse(echostr)
+            request.GET.get('echostr', ''), content_type="text/plain")
 
-        else:
+ 
 
-            return HttpResponse("weixin  token verify failed")
+ 
 
-    else:
+    # 解析本次请求的 XML 数据
 
-        xml_str = smart_str(request.body)
+    try:
 
-        print xml_str
-        
-        request_xml = etree.fromstring(xml_str)
+        wechat_instance.parse_data(data=request.body)
 
-        response_xml = auto_reply_main(request_xml)# 修改这里
+    except ParseError:
 
-        return HttpResponse(response_xml)
+        return HttpResponseBadRequest('Invalid XML Data')
+
+ 
+
+    # 获取解析好的微信请求信息
+
+    message = wechat_instance.get_message()
+
+ 
+
+    # 关注事件以及不匹配时的默认回复
+
+    response = wechat_instance.response_text(
+
+        content = (
+
+            '感谢您的关注！\n回复【功能】两个字查看支持的功能，还可以回复任意内容开始聊天'
+
+            '\n【<a href="http://www.ziqiangxuetang.com">自强学堂手机版</a>】'
+
+            ))
+
+    if isinstance(message, TextMessage):
+
+        # 当前会话内容
+
+        content = message.content.strip()
+
+        if content == '功能':
+
+            reply_text = (
+
+                    '目前支持的功能：\n1. 关键词后面加上【教程】两个字可以搜索教程，'
+
+                    '比如回复 "Django 后台教程"\n'
+
+                    '2. 回复任意词语，查天气，陪聊天，讲故事，无所不能！\n'
+
+                    '还有更多功能正在开发中哦 ^_^\n'
+
+                    '【<a href="http://www.ziqiangxuetang.com">自强学堂手机版</a>】'
+
+                )
+
+        elif content.endswith('教程'):
+
+            reply_text = '您要找的教程如下：'
+
+ 
+
+        response = wechat_instance.response_text(content=reply_text)
+
+ 
+
+    return HttpResponse(response, content_type="application/xml")
