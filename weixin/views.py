130a6c12a4d260134db11efd37f466e13145b556
pengfeng <pf0715479@163.com>
2017-02-09 00:43:55 +0800

    Xin Wen

--- weixin/views.py
+++ weixin/views.py
@@ -23,7 +23,19 @@ WECHAT_TOKEN = 'yuxuanyixiao'
 AppID = 'wx6c1c0226980f62e2'
 AppSecret = '5b29cdb78ab3cdd95972d7a9bee4cd24'
 
- 
+welcomeTitle = '欢迎关注公众号，点击查看我们的详细功能'
+logoUrl = 'http://mmbiz.qlogo.cn/mmbiz_jpg/WOBRP1ClHtJ94zJYlan7fW8ZWrSmxY6eYvf96Ricm5ToBbpib3Kia5R5yZmKia38sqzP7BJDmeeGLYY2Xrj4ZU9icpA/0?wx_fmt=jpeg'
+welcomeDescription = ('''
+我们非常聪明的机器人可以回答你很多问题啦～\n
+想听笑话，试试给我说 “讲个笑话呗”\n
+想查天气，试试给我说“苏州今天天气怎么样”\n
+想看新闻，试试给我说“刘德华的新闻”\n
+想做新菜，试试给我说 “回锅肉”\n
+想查快递，试试告诉我 “顺丰＋ 快递号”\n\n
+欢迎大家使用，更多功能正在开发中，敬请期待！\n
+请将宝贵意见发给我们.
+''')
+detailUrl = ''
 
 # 实例化 WechatBasic
 
@@ -67,15 +79,23 @@ def weixin_main(request):
     fromUser = message.source
     print  'fromUser : ', fromUser
     # 关注事件以及不匹配时的默认回复
-    response = wechat_instance.response_text(
-        content = (
-            '感谢您的关注！\n回复【功能】两个字查看支持的功能，还可以回复任意内容进行中英翻译[支持语音识别]\n'
-            '1. 回复【资讯】可以推送相关资讯.\n'
-            '2.回复新闻【头条新闻】【娱乐新闻】【科技新闻】【军事新闻】【财经新闻】【体育新闻】获取热门新闻'
-            '3.回复任意中英文词语，可以进行中英翻译[支持语音识别]\n'
-            '还有更多功能正在开发中哦，尽情期待，请将宝贵建议发送给我 ^_^\n'
-            '【<a href="http://www.pyuxuan.cn">轩轩一笑</a>】'
-           ))
+    # response = wechat_instance.response_text(
+    #     content = (
+    #         '感谢您的关注！\n回复【功能】两个字查看支持的功能，还可以回复任意内容进行中英翻译[支持语音识别]\n'
+    #         '1. 回复【资讯】可以推送相关资讯.\n'
+    #         '2.回复新闻【头条新闻】【娱乐新闻】【科技新闻】【军事新闻】【财经新闻】【体育新闻】获取热门新闻'
+    #         '3.回复任意中英文词语，可以进行中英翻译[支持语音识别]\n'
+    #         '还有更多功能正在开发中哦，尽情期待，请将宝贵建议发送给我 ^_^\n'
+    #         '【<a href="http://www.pyuxuan.cn">轩轩一笑</a>】'
+    #        ))
+    welcomeArticles = []
+    welcomeArticle = {}
+    welcomeArticle['title'] = welcomeTitle
+    welcomeArticle['description'] = welcomeDescription
+    welcomeArticle['picurl'] = logoUrl
+    welcomeArticle['url'] = None
+    welcomeArticles.append(welcomeArticle)
+    response = wechat_instance.response_news(welcomeArticles)
 
     if isinstance(message, TextMessage):
         # 当前会话内容
