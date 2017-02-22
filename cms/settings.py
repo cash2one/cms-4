3afe24e4f0a6076ee07e5c5478524c2e1174509f
pengfeng <pf0715479@163.com>
2017-02-22 23:27:07 +0800

    Theme

--- cms/settings.py
+++ cms/settings.py
@@ -198,7 +198,7 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [
-            os.path.join(PROJECT_ROOT, "nova/templates"),
+            os.path.join(PROJECT_ROOT, "solid/templates"),
             os.path.join(PROJECT_ROOT, "templates")
         ],
         "APP_DIRS": True,
@@ -232,7 +232,7 @@ if DJANGO_VERSION < (1, 9):
 
 INSTALLED_APPS = (
     "weixin",
-    "nova",
+    "solid",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
